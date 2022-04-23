5aab29e71ff224c3a1530e7a03e6df70239e6ca8
iscream <melscre4m@gmail.com>
2022-04-23 17:20:10 +0300

    Added Models

---
+++ TaskManager/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Введите логин")]
+        [StringLength(20, MinimumLength = 5, ErrorMessage = "Длина логина должна быть от 5 до 20 символов")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Введите пароль")]
+        [StringLength(30, MinimumLength = 8, ErrorMessage = "Длина пароля должна быть от 8 до 30 символов")]
+        public string Password { get; set; }
+
+        public List<Task> Tasks { get; set; }
+
+        public User()
+        {
+            Tasks = new List<Task>();
+        }
+    }
+}
