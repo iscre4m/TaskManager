bac081dfecf521fe9b4bc1a554d3184770d656fc
Nikita Miller <melscre4m@gmail.com>
2022-05-01 12:25:56 +0300

    Added End Date Picker

--- TaskManager/wwwroot/scripts.js
+++ TaskManager/wwwroot/scripts.js
@@ -1,10 +1,16 @@
-﻿function resetForms () {
-    $(document).find("form")[0].reset();
-    $(document).find("form")[1].reset();
+﻿function resetForms() {
+    $(document).find("#signInForm")[0].reset();
+    $(document).find("#signUpForm")[0].reset();
+    $(document).find("#addForm")[0].reset();
     $(this).find("span").empty();
 };
 
-$(function () {
+$(function() {
     $("#signUpModal").on("hidden.bs.modal", resetForms);
     $("#signInModal").on("hidden.bs.modal", resetForms);
+    $("#addModal").on("hidden.bs.modal", resetForms)
+    let maxDate = new Date();
+    maxDate.setFullYear(maxDate.getFullYear() + 1);
+    $("#addEndDate").attr("min", new Date().toISOString().split("T")[0]);
+    $("#addEndDate").attr("max", maxDate.toISOString().split("T")[0]);
 });
