912a685bead6b4e7a2eb7930f7c9a020cb756c65
Nikita Miller <melscre4m@gmail.com>
2022-05-04 10:09:31 +0300

    Fixed Remove

--- TaskManager/Controllers/MainController.cs
+++ TaskManager/Controllers/MainController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 using TaskManager.Data;
@@ -17,11 +19,20 @@ namespace TaskManager.Controllers
 
         public async Task<IActionResult> App()
         {
+            if (ViewBag.User is not null)
+            {
+                return View();
+            }
+
             User user = await _context.Users.FirstOrDefaultAsync(u => u.IsSignedIn == true);
 
             if (user is not null)
             {
                 await _context.Entry(user).Collection(u => u.Tasks).LoadAsync();
+                foreach(Models.Task task in user.Tasks)
+                {
+                    await _context.Entry(task).Collection("Subtasks").LoadAsync();
+                }
                 ViewBag.User = user;
 
                 return View();
@@ -32,9 +43,30 @@ namespace TaskManager.Controllers
             return View("Error");
         }
 
+        public async Task<IActionResult> Error()
+        {
+            User user = await _context.Users.FirstOrDefaultAsync(u => u.IsSignedIn == true);
+
+            if (user is not null)
+            {
+                return View();
+            }
+
+            return Redirect("/Home/Index");
+        }
+
         [HttpPost]
         public async Task<IActionResult> Add(Models.Task task)
         {
+            Models.Task queryTask = await _context.Tasks.FirstOrDefaultAsync(t => t.Description == task.Description);
+            
+            if (queryTask is not null)
+            {
+                ViewBag.Message = "Задача с таким описанием уже существует";
+
+                return View("Error");
+            }
+
             await _context.Tasks.AddAsync(task);
             (await _context.Users.FirstAsync(u => u.IsSignedIn == true)).Tasks.Add(task);
             await _context.SaveChangesAsync();
@@ -54,7 +86,14 @@ namespace TaskManager.Controllers
         [HttpPost]
         public async Task<IActionResult> Remove(Models.Task task)
         {
-            _context.Tasks.Remove(task);
+            Models.Task queryTask = await _context.Tasks.FindAsync(task.Id);
+            await _context.Entry(queryTask).Collection("Subtasks").LoadAsync();
+            
+            foreach (Subtask subtask in queryTask.Subtasks)
+            {
+                _context.Subtasks.Remove(subtask);
+            }
+            _context.Tasks.Remove(queryTask);
             await _context.SaveChangesAsync();
 
             return RedirectToAction("App", "Main");
--- TaskManager/wwwroot/scriptsJQuery.js
+++ TaskManager/wwwroot/scriptsJQuery.js
@@ -2,6 +2,7 @@
     $(document).find("#signInForm")[0].reset();
     $(document).find("#signUpForm")[0].reset();
     $(document).find("#addForm")[0].reset();
+    $(document).find("#editForm")[0].reset();
     $(this).find("span").empty();
     $(this).find("ul").empty();
 };
@@ -10,12 +11,18 @@ $(function() {
     $("#signUpModal").on("hidden.bs.modal", resetForms);
     $("#signInModal").on("hidden.bs.modal", resetForms);
     $("#addModal").on("hidden.bs.modal", resetForms);
+    $("#editModal").on("hidden.bs.modal", resetForms);
     
     let maxDate = new Date();
     maxDate.setFullYear(maxDate.getFullYear() + 1);
+    
     $("#addEndDate").attr("min", new Date().toISOString().split("T")[0]);
     $("#addEndDate").attr("value", $("#addEndDate").attr("min"));
     $("#addEndDate").attr("max", maxDate.toISOString().split("T")[0]);
+    
+    $("#editEndDate").attr("min", new Date().toISOString().split("T")[0]);
+    $("#editEndDate").attr("value", $("#editEndDate").attr("min"));
+    $("#editEndDate").attr("max", maxDate.toISOString().split("T")[0]);
     
     $("#addAddSubtask").click(function () {
         if ($("#addSubtask").val() === "") {
@@ -28,10 +35,12 @@ $(function() {
             }
         }
 
-        const closeButtonId = `removeSubtask${$("#addSubtasksList").children().length}`;
+        let subtasksCount = $("#addSubtasksList").children().length;
+        const closeButtonId = `removeSubtask${subtasksCount}`;
         
         $("#addSubtasksList").append(`<li class="list-group-item">
                                         <div class="d-flex justify-content-between">
+                                            <input type="hidden" name="Subtasks[${subtasksCount}].description" value="${$("#addSubtask").val()}">
                                             ${$("#addSubtask").val()}
                                             <input id="${closeButtonId}" type="button" class="btn-close">
                                         </div>
